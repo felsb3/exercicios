b0e3dfc8d24bcf738a74808088ac5843d04ad33a
felsb3 <felsb3@gmail.com>
2021-06-07 16:33:08 -0300

    executado exercicio 13

---
+++ pyntonexercicios/exes0013.py
@@ -0,0 +1,14 @@
+# Exercicio 13 -- Tendo como dado de entrada a altura (h) de uma pessoa, construa um algoritmo que calcule
+# seu peso ideal, utilizando as seguintes fórmulas: Para homens: (72.7h) - 58 Para mulheres: (62.1h) - 44.7
+
+h = float(input('Favor informar sua altura em metros: '))
+sexo = input('Digite aqui seu sexo "M" para masculino "F" para feminino: ').upper()
+if sexo == 'M':
+    peso_ideal = (72.7*h)-58
+    print(f' Para sexo Masculino seu peso ideal seria {peso_ideal} kg')
+elif sexo == "F":
+    peso_ideal = (62.1*h)-44.7
+    print(f'Para sexo Feminino seu peso ideal seria {peso_ideal} kg')
+else:
+    print('sexo invalido')
+print('Finalizou o programa')
