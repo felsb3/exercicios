f6af8083d83444b573396488dbc2b37558f012f8
felsb3 <felsb3@gmail.com>
2021-09-20 12:41:31 -0300

    Resolvidos exercícios 5

---
+++ pyntonexercicios/EstruturaSequencial/exercicio5.py
@@ -0,0 +1,11 @@
+# Estrutura Sequencial exercício 5 https://wiki.python.org.br/EstruturaSequencial
+# Faça um Programa que converta metros para centímetros.
+
+tamanho_em_metros = float(input('Incisa um valor em metros: '))
+tamanho_em_centimetros = tamanho_em_metros * 100
+print(f'Seu valor é igual a {tamanho_em_centimetros} cm')
+print()
+
+c = float(input('Insira os centímetros a serem convertidos:  '))
+r = c / 100
+print(f'{c} cm, é equivalente a: {r} metros')
