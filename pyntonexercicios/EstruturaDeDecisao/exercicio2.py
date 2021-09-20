a1c7affd1ef841fd9920dd3667ad59f3c80f2d65
felsb3 <felsb3@gmail.com>
2021-09-20 13:13:28 -0300

    Resolvidos exercícios Estrutura De Decisão 2

---
+++ pyntonexercicios/EstruturaDeDecisao/exercicio2.py
@@ -0,0 +1,13 @@
+# Estrutura De Decisão exercício 2 https://wiki.python.org.br/EstruturaDeDecisao
+# Faça um Programa que verifique se uma letra digitada é "F" ou "M".
+# Conforme a letra escrever: F - Feminino, M - Masculino, Sexo Inválido.
+sexo = input('Digite aqui seu sexo "M" para masculino "F" para feminino e "I" para não definir: ').upper()
+if sexo == 'M':
+    print('Masculino')
+elif sexo == "F":
+    print('Feminino')
+elif sexo == "I":
+    print('Indefinido')
+else:
+    print('sexo invalido')
+print('Finalizou o programa')
