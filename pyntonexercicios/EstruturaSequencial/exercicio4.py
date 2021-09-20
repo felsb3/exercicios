b8e4fc30f72d70747fed7c47a36da2c3105bf655
felsb3 <felsb3@gmail.com>
2021-09-20 12:37:20 -0300

    Resolvidos exercícios 4

---
+++ pyntonexercicios/EstruturaSequencial/exercicio4.py
@@ -0,0 +1,8 @@
+# Estrutura Sequencial exercício 4 https://wiki.python.org.br/EstruturaSequencial
+# Faça um Programa que peça as 4 notas bimestrais e mostre a média.
+nota_1 = float(input('Digite sua primeira nota: '))
+nota_2 = float(input('Digite sua segundo nota: '))
+nota_3 = float(input('Digite sua terciro nota: '))
+nota_4 = float(input('Digite sua quarta nota: '))
+meida = (nota_1 + nota_2 + nota_3 + nota_4)/4
+print(f'Sua média foi: {meida}')
--- pyntonexercicios/ex01es0001.py
+++ pyntonexercicios/ex01es0001.py
@@ -1,11 +1,4 @@
 # Estrutura Sequencial exercício 1 https://wiki.python.org.br/EstruturaSequencial
-# 4 Faça um Programa que peça as 4 notas bimestrais e mostre a média.
-nota_1 = float(input('Digite sua primeira nota: '))
-nota_2 = float(input('Digite sua segundo nota: '))
-nota_3 = float(input('Digite sua terciro nota: '))
-nota_4 = float(input('Digite sua quarta nota: '))
-meida = (nota_1 + nota_2 + nota_3 + nota_4)/4
-print(f'Sua média foi: {meida}')
 # 5 Faça um Programa que converta metros para centímetros.
 tamanho_em_metros = float(input('Incisa um valor em metros: '))
 tamanho_em_centimetros = tamanho_em_metros * 100
